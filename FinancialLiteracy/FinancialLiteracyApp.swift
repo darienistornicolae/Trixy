aa81064f9c7da2fc0ec7692aa6c514ef56fe0dcf
Darie-Nistor Nicolae <darie.nistornicolae@coolblue.nl>
2024-10-29 16:37:24 +0100

    Initial Commit

---
+++ FinancialLiteracy/FinancialLiteracyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialLiteracyApp.swift
+//  FinancialLiteracy
+//
+//  Created by Darie-Nistor Nicolae on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinancialLiteracyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
